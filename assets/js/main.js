7d48dd1c42bc85a073ffb89c0757ac14f770a9d2
aman-vishen <amanvishen46@gmail.com>
2022-08-04 17:38:59 +0530

    setting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,141 @@
-particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 355,
-        "density": {
-          "enable": true,
-          "value_area": 789.1476416322727
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.48927153781200905,
-        "random": false,
-        "anim": {
-          "enable": true,
-          "speed": 0.2,
-          "opacity_min": 0,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 2,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 2,
-          "size_min": 0,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 0.2,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+window.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+// output is in the range of [0, 1]
+// t is the elapsed time and d is the duration
+Math.linearInOut = function(t, d) {
+  if (t > d / 2) {
+    return 2 - (t / (d / 2));
+  } else {
+    return t / (d / 2);
+  }
+}
+
+window.onload = function() {
+  DoCanvasEffect('canvas');
+}
+
+function DoCanvasEffect(canvas_id) {
+  // scene options:
+  var follow = true;
+  var follow_opacity = true;
+  var speed = .20;
+
+  var canvas = document.getElementById(canvas_id);
+  var dt = 16.667;
+
+  //Make the canvas occupy the full page
+  var W = window.innerWidth,
+    H = window.innerHeight;
+  canvas.width = W;
+  canvas.height = H;
+  var ctx = canvas.getContext("2d");
+
+  var particles = [];
+  var mouse = {};
+
+  // track mouse
+  canvas.addEventListener('mousemove', function(e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+  }, false);
+
+  // create particles
+  var total = 50;
+  for (var i = 0; i < total; i++) {
+    particles.push(new particle(true));
+  }
+
+  function particle(dead) {
+    var a = Math.PI * 2 * Math.random();
+    this.speed = {
+      x: Math.cos(a) * 6 * speed,
+      y: Math.sin(a) * 6 * speed
+    };
+
+    // location = mouse coordinates
+    // Now the flame follows the mouse coordinates
+    if (mouse.x && mouse.y && follow) {
+      this.location = {
+        x: mouse.x + -150 + Math.random() * 300,
+        y: mouse.y + -150 + Math.random() * 300
+      };
+    } else {
+      this.location = {
+        x: W / 2,
+        y: H / 2
+      };
+    }
+
+    // radius range = 10-30
+    this.radius = 2 + Math.random() * 58;
+    if (dead) this.radius = 0;
+
+    // life range = 1500-5000ms
+    this.time = 1500 + Math.random() * 3500;
+    this.duration = this.time;
+
+    // color
+    this.r = Math.round(Math.random() * 50);
+    this.b = 150 + Math.round(Math.random() * 105);
+    this.g = 0; //Math.round(Math.random()*255);
+  }
+
+  function draw() {
+    if ((!mouse.x || !mouse.y) && follow_opacity) {
+      window.requestAnimFrame(draw);
+      return;
+    }
+
+    // clear the canvas with an image:
+    ctx.clearRect(0, 0, W, H);
+
+    // draw particles:
+    for (var i = 0; i < particles.length; i++) {
+      var p = particles[i];
+      ctx.beginPath();
+      var x = p.location.x,
+        y = p.location.y;
+      var o = Math.linearInOut(p.time, p.duration) * 0.50;
+      if (follow_opacity) {
+        var dx = mouse.x - x;
+        var dy = mouse.y - y;
+        var dist = (Math.sqrt(dx * dx + dy * dy) || 200);
+        o *= 1 - (dist / 200);
       }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "bubble"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 83.91608391608392,
-          "size": 1,
-          "duration": 3,
-          "opacity": 1,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      p.opacity = o != NaN ? o : p.opacity;
+
+      var gradient = ctx.createRadialGradient(x, y, 0, x, y, p.radius);
+      gradient.addColorStop(0, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
+      gradient.addColorStop(0.8, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
+      gradient.addColorStop(1, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", 0)");
+      ctx.fillStyle = gradient;
+      ctx.arc(p.location.x, p.location.y, p.radius, 0, Math.PI * 2, false);
+      ctx.fill();
+      ctx.closePath();
+
+      // some basic physics to move and animate the particles
+      p.time -= dt;
+      p.radius -= 0.25;
+      p.location.x += p.speed.x;
+      p.location.y += p.speed.y;
+
+      // check if the particle has finished:
+      if (p.radius < 0 || p.time < 0) {
+        particles[i] = new particle(); // reset it
       }
-    },
-    "retina_detect": true
-  });
+    }
+
+    window.requestAnimFrame(draw);
+  }
+
+  window.requestAnimFrame(draw);
+}
